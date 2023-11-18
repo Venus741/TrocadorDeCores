3c6c3fc78b276bd8d21f2063e4751b148d1f4eff
Venus741 <124938461+Venus741@users.noreply.github.com>
2023-11-18 12:07:34 -0300

    Adição de nova funcionalidade
    Um campo input que recebe a referencia da cor informada e mostra ela na div secundária.

--- Src/index.js
+++ Src/index.js
@@ -1,12 +1,20 @@
-let div = document.querySelector(".trocador"); // seleciona a classe da div que será trabalhada
+let divTrocador = document.querySelector(".trocador"); // seleciona a classe da div que será trabalhada
 let cores = ["#0000FF", "#FFFF00", "#008000"]; // váriável que contém as cores selecionadas para alteração
 let corAtual = 0; // posição no index da div
 
 function trocador() { 
     let alterar = cores[corAtual]; // nova variável chamando a cor da array que está no index "0"
 
-    div.style.backgroundColor = alterar; // Div recebendo váriável de alteração anterior
+    divTrocador.style.backgroundColor = alterar; // Div recebendo váriável de alteração anterior
 
     
     corAtual = (corAtual +1) % cores.length; // "corAtual" recebendo ela mesma + próxima cor da array. O operador "%" faz com que o resto da divisão retorne pra zero, quando chegar no limite da array. Sempre seguindo a sequencia da esquerda para a direita.
+}
+
+let divMostrador = document.querySelector(".identificador"); // seleciona a classe da div que será trabalhada
+let input = document.querySelector(".input"); // recebe o valor informado no input
+
+function encontrar() {
+    let receber = input.value; // converte o input em valor legível para ser trabalhado
+    divMostrador.style.backgroundColor = receber; // aplicação de método para o valor do input alterar o CSS
 }
